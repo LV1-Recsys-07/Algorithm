bf400d37be95c481c39b09388d60ef2f1cca2b0e
MSGitt <121923924+MSGitt@users.noreply.github.com>
2023-03-13 15:15:51 +0900

    Update 단지번호붙이기.py

--- 민수/1주차/2667. 단지번호붙이기/단지번호붙이기.py
+++ 민수/1주차/2667. 단지번호붙이기/단지번호붙이기.py
@@ -6,6 +6,7 @@ n = int(sys.stdin.readline())
 matrix = [list(map(int, sys.stdin.readline().rstrip())) for i in range(n)]
 visited = [[0] * n for i in range(n)]
 
+# queue를 이용한 bfs
 def bfs(x, y, matrix, visited, n) :
     
     row = [1, -1, 0, 0]
@@ -22,11 +23,11 @@ def bfs(x, y, matrix, visited, n) :
         for i in range(4) :
             newx = a + row[i]
             newy = b + col[i]
-            
+            # 4방향으로 탐색 진행
             if 0 <= newx < n  and  0 <= newy < n  and visited[newx][newy] == 0 :
                 visited[newx][newy] = 1
-                if matrix[newx][newy] == 1 :
-                    matrix[newx][newy] = 0
+                if matrix[newx][newy] == 1 :  # 1인 지점을 모두 0으로 바꿈
+                    matrix[newx][newy] = 0 
                     queue.append((newx, newy))
                     count += 1
                     
@@ -34,6 +35,7 @@ def bfs(x, y, matrix, visited, n) :
 
 result = []
 
+# matrix를 돌며 bfs 수행할 때 마다 result에 append
 for i in range(n) :
     for j in range(n) :
         if matrix[i][j] == 1 :
@@ -43,6 +45,4 @@ result.sort()
 
 print(len(result))
 for i in result :
-    print(i)
-        
-    
+    print(i)   
