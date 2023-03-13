ca8daae88050c88a33b97b491e3281f314b9da6c
MSGitt <121923924+MSGitt@users.noreply.github.com>
2023-03-13 15:13:27 +0900

    Update 바이러스.py

--- 민수/1주차/2606. 바이러스/바이러스.py
+++ 민수/1주차/2606. 바이러스/바이러스.py
@@ -1,17 +1,17 @@
 import sys
 
-
 n = int(sys.stdin.readline())
 m = int(sys.stdin.readline())
 
-vertex = [ []* n for i in range(n + 1)]
+vertex = [[]* n for i in range(n + 1)]
 
 for i in range(m) :
     a, b = map(int, sys.stdin.readline().split())
     
     vertex[a].append(b)
     vertex[b].append(a) 
-    
+
+# stack을 이용한 dfs로 탐색    
 def dfs(start, vertex) : 
 
     visited = []
@@ -21,11 +21,10 @@ def dfs(start, vertex) :
         v = stack.pop()
         if v not in visited : 
             visited.append(v)
-            for w in vertex[v] :
+            for w in vertex[v] : # 연결되어 있는 점들만 stack에 들어감
                 stack.append(w) 
                 
     return visited
             
 a = dfs(1, vertex)
 print(len(a) - 1)
- 
