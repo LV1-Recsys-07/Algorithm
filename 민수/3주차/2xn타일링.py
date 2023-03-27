57496a495c24bb49a5522215d05d59bce0d5729a
MS <121923924+MSGitt@users.noreply.github.com>
2023-03-27 15:39:46 +0900

    Update 2xn타일링.py

--- 민수/3주차/2xn타일링.py
+++ 민수/3주차/2xn타일링.py
@@ -2,12 +2,12 @@ import sys
 
 n = int(sys.stdin.readline())
 
-dp = [0] * 1001
+dp = [0] * 1001 # dp 리스트 생성
 
-dp[1] = 1
-dp[2] = 2
+dp[1] = 1 # 2 x 1 case 
+dp[2] = 2 # 2 x 2 case
 
 for i in range(3, n + 1) :
-    dp[i] = (dp[i-1] + dp[i-2]) % 10007
+    dp[i] = (dp[i-1] + dp[i-2]) % 10007 # 점화식으로 풀이
     
 print(dp[n])
