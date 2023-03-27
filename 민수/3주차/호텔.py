ba7d80329e3bd7c2f9bcdfb401ae391ba837ee50
MS <121923924+MSGitt@users.noreply.github.com>
2023-03-27 15:55:14 +0900

    Update 호텔.py

--- 민수/3주차/호텔.py
+++ 민수/3주차/호텔.py
@@ -3,8 +3,12 @@ import sys
 n, m = map(int, sys.stdin.readline().split())
 cost = [list(map(int, sys.stdin.readline().split())) for i in range(m)] 
 
+# 얻을 수 있는 고객의 수가 100보다 작거나 같은 자연수
+
 dp = [float('inf')] * (n + 100)
 dp[0] = 0
+
+# 늘어나는 고객수에 따른 최소 비용 업데이트
 
 for i, j in cost :
     for k in range(j, n + 100) :
