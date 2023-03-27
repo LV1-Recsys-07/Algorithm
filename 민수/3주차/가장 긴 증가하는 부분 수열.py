01fdefa78740b9319a3f31f8c647ccaf6d57aa4e
MS <121923924+MSGitt@users.noreply.github.com>
2023-03-27 15:51:23 +0900

    Update 가장 긴 증가하는 부분 수열.py

--- 민수/3주차/가장 긴 증가하는 부분 수열.py
+++ 민수/3주차/가장 긴 증가하는 부분 수열.py
@@ -3,7 +3,9 @@ import sys
 n = int(sys.stdin.readline())
 numbers = list(map(int, sys.stdin.readline().split()))
 
-dp = [1] * n 
+dp = [1] * n # dp 배열 생성
+
+# for문을 돌면서 i가 이전 수보다 크다면 업데이트
 
 for i in range(1, n) :
     for j in range(i) :
