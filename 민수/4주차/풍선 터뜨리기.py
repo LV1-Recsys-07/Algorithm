3f55e07d1339ad710e21ee106f798a9953625602
MS <121923924+MSGitt@users.noreply.github.com>
2023-04-03 15:50:20 +0900

    Update 풍선 터뜨리기.py

--- 민수/4주차/풍선 터뜨리기.py
+++ 민수/4주차/풍선 터뜨리기.py
@@ -4,18 +4,18 @@ from collections import deque
 n = int(sys.stdin.readline())
 number = list(map(int, sys.stdin.readline().split()))
 
-queue = deque((i + 1, n) for i, n in enumerate(number))
+queue = deque((i + 1, n) for i, n in enumerate(number)) # 몇 번째 풍선인지도 같이 넣어주기
 result = []
 
 for i in range(n) :
     
-    temp = queue.popleft()
+    temp = queue.popleft() # 1번 풍선 터트리기
     result.append(temp[0])
 
-    if temp[1] >= 0 :
-        queue.rotate(-(temp[1] - 1))
+    if temp[1] >= 0 : # 0보다 크다면 왼쪽으로 queue 회전
+        queue.rotate(-(temp[1] - 1)) # popleft 하였으므로 자동으로 1만큼 이동했기 때문에 -1 한 만큼 이동
 
     else :
-        queue.rotate(-temp[1])
+        queue.rotate(-temp[1]) # 오른쪽으로 queue 회전
 
 print(*result) 
