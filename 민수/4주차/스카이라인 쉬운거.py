3241af7dd75986559e70e0556caa333ce4b5f16b
MS <121923924+MSGitt@users.noreply.github.com>
2023-04-03 15:59:08 +0900

    Update 스카이라인 쉬운거.py

--- 민수/4주차/스카이라인 쉬운거.py
+++ 민수/4주차/스카이라인 쉬운거.py
@@ -6,16 +6,16 @@ stack = []
 result = 0
 
 for i in range(n) : 
-    x, y = map(int, sys.stdin.readline().split())
-    while stack and y < stack[-1] :
+    x, y = map(int, sys.stdin.readline().split()) # 좌표 받아오기
+    while stack and y < stack[-1] : # stack이 비어있지 않고 현재 좌표보다 높다면 pop
         result += 1
         stack.pop()
-    if stack and y == stack[-1] :
+    if stack and y == stack[-1] : # 현재 높이와 같다면 넘어가기
         continue 
         
-    stack.append(y)
+    stack.append(y) # continue에서 걸리지 않는다면 append
     
-while stack :
+while stack : # 남아 있는 건물 확인
     if stack[-1] > 0 :
         result += 1
     
